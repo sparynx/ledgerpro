1f37c99b8a05336905859fc2bbcf125f9b140a71
timileyin adebisi <sparynxcyberneti@gmail.com>
2025-08-05 15:43:18 +0100

    ignored type

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config) => {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
