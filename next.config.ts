22b87b1f1faae6c7e79f7ef055238ede2aa7ae25
timileyin adebisi <sparynxcyberneti@gmail.com>
2025-08-05 15:20:55 +0100

    add

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from "next";
 import path from 'path';
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
